98860da1f818323b8487dfce2e4225e45279428b
Crystal Oscillator <78525180+unreliablecode@users.noreply.github.com>
2023-09-05 00:35:18 +0700

    show better windows name

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
 #include <windows.h>
+#include <Shlwapi.h>
+
+// Function to get the friendly name of the operating system
+std::string GetOSFriendlyName() {
+    std::string result;
+    HKEY hKey;
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        char buffer[MAX_PATH];
+        DWORD bufferSize = sizeof(buffer);
+        if (RegQueryValueEx(hKey, "ProductName", nullptr, nullptr, (LPBYTE)buffer, &bufferSize) == ERROR_SUCCESS) {
+            result = buffer;
+        }
+        RegCloseKey(hKey);
+    }
+    return result;
+}
+
+// Function to activate Windows using a product key
+bool ActivateWindows(const std::string& productKey) {
+    std::string activationCommand = "slmgr.vbs /ipk " + productKey;
+
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    PROCESS_INFORMATION pi;
+
+    if (CreateProcess(NULL, const_cast<char*>(activationCommand.c_str()), NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        return true;
+    }
+    return false;
+}
 
 int main() {
+    std::string osName = GetOSFriendlyName();
+    std::cout << "Operating System: " << osName << std::endl;
+
     std::cout << "Enter the Windows product key: ";
     std::string productKey;
-    std::getline(std::cin, productKey);
+    std::cin >> productKey;
 
     if (ActivateWindows(productKey)) {
         std::cout << "Windows has been successfully activated!" << std::endl;
@@ -17,29 +50,4 @@ int main() {
     std::cout << "Press any key to exit...";
     std::cin.get();
     return 0;
-}
-
-bool ActivateWindows(const std::string& productKey) {
-    std::string activationCommand = "slmgr.vbs /ipk " + productKey;
-
-    try {
-        SHELLEXECUTEINFO shExecInfo = {0};
-        shExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-        shExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-        shExecInfo.hwnd = NULL;
-        shExecInfo.lpVerb = L"runas";  // Run as administrator
-        shExecInfo.lpFile = L"cmd.exe";
-        shExecInfo.lpParameters = (LPWSTR)activationCommand.c_str();
-        shExecInfo.nShow = SW_HIDE;     // Hide the Command Prompt window
-
-        if (ShellExecuteEx(&shExecInfo)) {
-            WaitForSingleObject(shExecInfo.hProcess, INFINITE);
-            CloseHandle(shExecInfo.hProcess);
-            return true;
-        } else {
-            return false;
-        }
-    } catch (...) {
-        return false;
-    }
 }
